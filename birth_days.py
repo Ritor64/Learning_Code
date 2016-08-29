21fff4031d3b8c60248aa1ecac9a0044dec40e9a
Ritor64 <diverty@gmail.com>
2016-08-29 12:35:11 +0300

    Add files via upload

---
+++ birth_days.py
@@ -0,0 +1,7 @@
+# while True:
+#     for i in ["/","- ","|","\\","|"]:
+#         print("%s\r" % i),
+
+print('How old are you?')
+age = input()
+print(age)
---
+++ dec2hex.py
@@ -0,0 +1,4 @@
+def dec2hex(num): 
+    print ("0x%02x" % num)
+    
+dec2hex(15)
